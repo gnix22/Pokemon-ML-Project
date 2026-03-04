c353e6a31dee29b7ce2c2012c8be346b789b12aa
gnix22 <gjnix22@gmail.com>
2026-03-04 09:49:28 -0500

    data cleaning, finding correlations

--- src/data-cleaning.py
+++ src/data-cleaning.py
@@ -2,9 +2,20 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv("../data/pokemon_data.csv")
-print(df.head(5))
-print(df.columns)
+# get hot one encoding of dual type to use in corr matrix
+df["combined_typing"] = df["Type 1"] + '/' + df["Type 2"]
+one_hot_encoded_combined = pd.get_dummies(df["combined_typing"],dtype='uint8')
+list_encoded = one_hot_encoded_combined.columns.to_list()
+df.drop('combined_typing', axis=1)
+df = df.join(one_hot_encoded_combined)
+
 # can exclude pretty much everything other than stats and typing
-keep_col_list = ["Name","Attack","Defense","Sp. Attack","Sp. Defense","Speed","Base_Stats","Type 1","Type 2"]
+# this is due to the fact that Base_Stats are total of individual stats
+keep_col_list = ["Name","Base_Stats","Type 1","Type 2","number_immune","number_not_effective","number_normal","number_super_effective"]
+keep_col_list.extend(list_encoded)
 selected_df = df[keep_col_list]
-print(selected_df.head(5))
+only_numeric = selected_df.select_dtypes(include=[np.number])
+
+correlation = only_numeric.corr()
+print("correlation matrix: ")
+print(correlation)
