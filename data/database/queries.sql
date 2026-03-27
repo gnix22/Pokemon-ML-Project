5a1cfbbd12ba7de9d69c902a6382edde3c0793ce
gnix22 <gjnix22@gmail.com>
2026-03-27 12:28:50 -0400

    attempt to get data on competitive pokemon from prev wins

--- data/database/queries.sql
+++ data/database/queries.sql
@@ -3,3 +3,6 @@
 SELECT COUNT(*) FROM "pokemon";
 SELECT COUNT(*) FROM "moves";
 SELECT COUNT(*) FROM "poke-moves";
+
+-- create specific roles that are commonly seen in competitive pokemon
+-- this will eventually assign roles to every pokemon.
